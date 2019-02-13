42f38e44ba4fdcb0856304dbf8c94fa73d3d3dab
danieltalhaug <daniel.talhaug@outlook.com>
2019-02-13 14:35:08 +0100

    added browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,27 @@ var gulp = require('gulp');
 // Include Plugins
 var less = requre('gulp-less');
 var path = require('path');
+var browserSync = require('browser-sync');
 
 // Less
 gulp.task('less', function () {
     return gulp.src('less/*.less')
-      .pipe(less({
+    .pipe(less({
         paths: [ path.join(__dirname, 'less', 'includes') ]
-      }))
-      .pipe(gulp.dest('dist/css'));
-  });
+    }))
+    .pipe(gulp.dest('dist/css'));
+});
+
+// BrowserSync
+gulp.task('browser-sync', function() {
+    browserSync.init(['css/*.css'], {
+        server: {
+            baseDir: './'
+        }
+    });
+});
 
 // Watch files for changes
-gulp.task('watch', function() {
+gulp.task('default', ['less', 'browser-sync'], function() {
     gulp.watch('less/*.less', ['less']);
 })
-
-// Default task
-gulp.task('default', ['less']);
